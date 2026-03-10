0ae9c97940e35f9cdd0e425a7c55de0c9991d119
Wild-Dandelion <sm5756@srmist.edu.in>
2026-03-10 09:04:53 +0530

    UC1: Application Entry & Welcome Message

---
+++ HotelBookingApp.java
@@ -0,0 +1,11 @@
+/*
+Usercase 1: Application Entry & Welcome Message
+* */
+
+
+public class HotelBookingApp{
+    public static void main(String [] args){
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
