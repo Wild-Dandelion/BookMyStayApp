f66d690d57572984cf2bf34f069a1bed45bb701f
Wild-Dandelion <sm5756@srmist.edu.in>
2026-03-10 09:38:29 +0530

    UC2: Basic Room Types & Static Availability

--- HotelBookingApp.java
+++ HotelBookingApp.java
@@ -1,13 +1,54 @@
 /*
-*Usercase 1: Application Entry & Welcome Message
+*Usercase 2: Basic Room Types & Static Availability
 * @author Shikher
-* @version 1.0
+* @version 2.1
 */
 
 
+abstract class Room{
+    protected int numberOfBeds;
+    protected int sqaureFeet;
+    protected double pricePerNight;
+    protected static int availabiltiy = 6;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight){
+        this.numberOfBeds = numberOfBeds;
+        this.sqaureFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+        availabiltiy-=1;
+    }
+    public void displayRoomDetails(){
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Size of the Room: " + sqaureFeet);
+        System.out.println("Rate of the Room: " + pricePerNight);
+        System.out.println("Availability: " + availabiltiy);
+    }
+}
+
+class SingleRoom extends Room{
+    public SingleRoom(){super(1,250,1500.00);}
+}
+
+class DoubleRoom extends Room{
+    public DoubleRoom(){
+        super(2,400,2500.0);
+    }
+}
+class SuiteRoom extends Room{
+    public SuiteRoom(){
+        super(3,750,5000.0);
+    }
+}
+
 public class HotelBookingApp{
     public static void main(String [] args){
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        SingleRoom sr = new SingleRoom();
+        sr.displayRoomDetails();
+        System.out.println("\n");
+        DoubleRoom dr = new DoubleRoom();
+        dr.displayRoomDetails();
+        System.out.println("\n");
+        SuiteRoom sur = new SuiteRoom();
+        sur.displayRoomDetails();
     }
 }
